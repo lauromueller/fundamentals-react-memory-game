f303a396916aa17cb3a282e140e667da134063ff
Lauro Fialho Müller <contact@lauromueller.com>
2023-02-11 16:10:58 +0100

    Added card flipping and winning game logic

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from './Card';
 
 type BoardCard = {
@@ -10,27 +10,55 @@ const Board = () => {
   const [cards, setCards] = useState<BoardCard[]>([
     {
       color: '#f00',
-      isFlipped: true,
+      isFlipped: false,
     },
     {
       color: '#0f0',
       isFlipped: false,
     },
     {
-      color: '#00f',
+      color: '#f00',
       isFlipped: false,
     },
   ]);
 
+  useEffect(() => {
+    const flippedCards = cards.filter((card) => card.isFlipped);
+
+    if (flippedCards.length === 2) {
+      if (flippedCards[0].color === flippedCards[1].color) {
+        setTimeout(() => window.alert('You won!'), 50);
+      } else {
+        setTimeout(
+          () =>
+            setCards(
+              cards.map((card) => ({
+                ...card,
+                isFlipped: false,
+              }))
+            ),
+          2000
+        );
+      }
+      // If cards[cardIndex].color === flippedCards[0].color -> Game won!
+      // Otherwise, we flip the second card and we set a timer of, say,
+      // 2 seconds to unflip the cards.
+    }
+  }, [cards]);
+
   const flipCard = (cardIndex: number) => {
-    setCards([
-      ...cards.slice(0, cardIndex),
-      {
-        isFlipped: !cards[cardIndex].isFlipped,
-        color: cards[cardIndex].color,
-      },
-      ...cards.slice(cardIndex + 1, cards.length),
-    ]);
+    const flippedCards = cards.filter((card) => card.isFlipped);
+
+    if (flippedCards.length < 2) {
+      setCards([
+        ...cards.slice(0, cardIndex),
+        {
+          isFlipped: !cards[cardIndex].isFlipped,
+          color: cards[cardIndex].color,
+        },
+        ...cards.slice(cardIndex + 1, cards.length),
+      ]);
+    }
   };
 
   return (
